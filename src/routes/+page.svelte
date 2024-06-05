cf828d44bd363573d5c5cc25f5524543a1b388dc
lnjng <lnjng07@protonmail.com>
2024-06-04 17:32:39 -0700

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<div class="bg-slate-400">
-    How can we <em>help</em> you today?
+<div class="relative min-h-80 bg-slate-400">
+    <img class="min-h-80 object-cover" src="https://fastly.picsum.photos/id/867/4000/700.jpg?hmac=iqtwmtRJSsyaIQw9vhTT04QCVcOjVJz5jAh1nRHqadA" alt="placeholder" />
+    <div class="absolute top-1/2 left-1/2 text-white text-6xl transform -translate-x-1/2 -translate-y-1/2">How can we selp you today?</div>
 </div>
